2441b77435f7011b4d6581c1e05a546f0275dc5a
Luis Ake <luis.ake142002@gmail.com>
2024-09-02 14:01:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_luis_ake
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_luis_ake/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_luis_ake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
